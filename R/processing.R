9543a6177102ddec810e77768866ae39172728d5
lukakoning <koningluka@gmail.com>
2025-08-20 11:22:24 +0200

    mark text modes: fix issue with functions for fuzzy matching not being available in async process

--- R/processing.R
+++ R/processing.R
@@ -893,7 +893,12 @@ processing_server <- function(
             write_paragraph = write_paragraph,
             write_paragraphs = write_paragraphs(),
             text_size_tokens = context_window$max_tokens,
-            overlap_size_tokens = context_window$overlap
+            overlap_size_tokens = context_window$overlap,
+            find_matches = find_matches,
+            normalize_with_map = normalize_with_map,
+            best_literal_substring = best_literal_substring,
+            fuzzy_threshold = fuzzy_threshold,
+            normalize_for_dist = normalize_for_dist
           ),
           packages = c(
             "tidyprompt",
